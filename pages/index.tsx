c43d12c2f004761aa4745626aabe91211304c2d2
박소예 <soyepark@bagsoyes-MacBook-Air.local>
2023-05-26 21:34:52 +0900

    Chore : 파비콘 & 타이틀 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,19 @@ import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
-  return <h1>Refilled Free Assignment</h1>;
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://s3.ap-northeast-2.amazonaws.com/refilled.co.kr/favicon/symbol_only.png"
+        ></link>
+        <title>리필드</title>
+      </Head>
+      <h1>Refilled Free Assignment</h1>
+    </>
+  );
 };
 
 export default Home;

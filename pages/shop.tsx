4d40d13bbbb508d9a15c9cd8637b72b00c223a1a
박소예 <soyepark@bagsoyes-MacBook-Air.local>
2023-05-27 00:37:27 +0900

    Style : Copy 마크업 및 스타일링

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,3 +1,5 @@
+import styles from "../styles/Shop.module.scss";
+
 import React from "react";
 import Header from "../src/components/Header";
 
@@ -5,6 +7,10 @@ const shop = () => {
   return (
     <>
       <Header />
+      <div className={styles.copy}>
+        사이토카인. <br />
+        <span>완벽한 탈모케어를 위한 선택</span>
+      </div>
     </>
   );
 };

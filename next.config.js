f95539d31c6e76971958edd74646488c32563365
박소예 <soyepark@bagsoyes-MacBook-Air.local>
2023-05-26 23:47:52 +0900

    style : scss 옵션 추가
    - mixins 파일 생성

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`,
   },
 };
 

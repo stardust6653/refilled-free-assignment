85a8602cad8593ba7fd2af76c8f2dc9db4121406
박소예 <soyepark@bagsoyes-MacBook-Air.local>
2023-05-27 00:08:24 +0900

    feat : Header 마크업 및 스타일링

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
@@ -14,15 +13,6 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Head>
-        <link
-          rel="icon"
-          type="image/png"
-          href="https://s3.ap-northeast-2.amazonaws.com/refilled.co.kr/favicon/symbol_only.png"
-        ></link>
-        <title>리필드</title>
-      </Head>
-
       <h1>Refilled Free Assignment</h1>
       <p>{count}</p>
       <button
--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,7 +1,59 @@
+import styles from "../styles/Shop.module.scss";
+
+import Image from "next/image";
 import React from "react";
 
 const shop = () => {
-  return <div>shop</div>;
+  return (
+    <>
+      <div className={styles.component}>
+        <div className={styles.status}>
+          <Image
+            className={styles.time}
+            src="/../public/assets/time.png"
+            alt="9시 41분"
+            width={28.43}
+            height={11.09}
+          />
+          <Image
+            className={styles.side}
+            src="/../public/assets/top-side.png"
+            alt="상단 사이드 바"
+            width={66.6}
+            height={11.34}
+          />
+        </div>
+        <header>
+          <nav>
+            <Image
+              src="/../public/assets/menu.png"
+              alt="메뉴로 가기"
+              width={40}
+              height={40}
+            />
+          </nav>
+          {/* <h1 className="hiddin">Refilled</h1> */}
+          <div className={styles.logo}>
+            <Image
+              src="/../public/assets/logo.png"
+              alt="Refilled"
+              width={100}
+              height={24}
+            />
+          </div>
+
+          <div className={styles.cart}>
+            <Image
+              src="/../public/assets/cart.png"
+              alt="장바구니로 가기"
+              width={22}
+              height={21}
+            />
+          </div>
+        </header>
+      </div>
+    </>
+  );
 };
 
 export default shop;

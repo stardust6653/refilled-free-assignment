4a39c3c28fa4e816463743e51f7d9f7e90518699
박소예 <soyepark@bagsoyes-MacBook-Air.local>
2023-05-26 22:30:53 +0900

    feat : Redux toolkit 세팅 및 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,15 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
+import { decrement, increment } from "../src/features/counter/counterSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../src/app/store";
+
 const Home: NextPage = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+
+  const dispatch = useDispatch();
+
   return (
     <>
       <Head>
@@ -14,7 +22,23 @@ const Home: NextPage = () => {
         ></link>
         <title>리필드</title>
       </Head>
+
       <h1>Refilled Free Assignment</h1>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        증가
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        감소
+      </button>
     </>
   );
 };

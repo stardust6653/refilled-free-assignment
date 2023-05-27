5c65d78ad80061921c3f843bca45c3aa59f0406d
박소예 <soyepark@bagsoyes-MacBook-Air.local>
2023-05-28 00:02:12 +0900

    feat : 옵션 선택하지 않았을 시 노출되는 문구 추가

---
+++ src/components/Alert.tsx
@@ -0,0 +1,13 @@
+import styles from "../../styles/Alert.module.scss";
+
+import React from "react";
+
+const Alert = () => {
+  return (
+    <>
+      <p className={styles.alert}>옵션을 먼저 선택해주세요.</p>
+    </>
+  );
+};
+
+export default Alert;

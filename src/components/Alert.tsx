a805d71a4304cd2b0f773f3dc600c0c8ea16f5e3
박소예 <soyepark@bagsoyes-MacBook-Air.local>
2023-05-29 13:55:41 +0900

    Style : alert 위치 고정
    - absolute 를 활용한 위치고정

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Alert = () => {
   return (
     <>
-      <p className={styles.alert}>옵션을 먼저 선택해주세요.</p>
+      <p className={styles.alert}>옵션을 먼저 선택해주세요</p>
     </>
   );
 };

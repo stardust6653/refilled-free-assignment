1a0a61bbb95dbefc5e71a16ac65d1cd4f4c8db2e
박소예 <soyepark@bagsoyes-MacBook-Air.local>
2023-05-29 13:54:15 +0900

    Style : 테스트 마크업 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,10 @@
 import type { NextPage } from "next";
-import Image from "next/image";
-import styles from "../styles/Home.module.scss";
-
-import { decrement, increment } from "../src/features/counter/counterSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../src/app/store";
+import Header from "../src/components/Header";
 
 const Home: NextPage = () => {
-  const count = useSelector((state: RootState) => state.counter.value);
-
-  const dispatch = useDispatch();
-
   return (
     <>
-      <h1>Refilled Free Assignment</h1>
-      <p>{count}</p>
-      <button
-        onClick={() => {
-          dispatch(increment());
-        }}
-      >
-        증가
-      </button>
-      <button
-        onClick={() => {
-          dispatch(decrement());
-        }}
-      >
-        감소
-      </button>
+      <Header />
     </>
   );
 };

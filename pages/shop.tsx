a7e5547f61ffa4f895894c87100ce47ff6a07c77
Soye Park <stardust6653@gmail.com>
2023-05-30 15:55:38 +0900

    style : DesktopPopUp 층위 변경
    - 가장 최상위로 변경

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -17,29 +17,31 @@ const Shop = ({ data }: { data: CardItemProps[] }) => {
   const modal = useSelector((state: RootState) => state.modal.value);
 
   return (
-    <div className={styles.layout}>
+    <>
       <DesktopPopUp />
-      <div className={styles.component}>
-        <Header />
-        <div className={styles.copy}>
-          사이토카인. <br />
-          <span>완벽한 탈모케어를 위한 선택</span>
-        </div>
-        <div className={styles.cardList}>
-          <CardList list={data} />
-        </div>
-        {modal && <Select />}
-        <div className={styles.toastLayout}>
-          <ToastContainer
-            className={styles.toast}
-            position="bottom-center"
-            limit={2}
-            autoClose={1000}
-            hideProgressBar={true}
-          />
+      <div className={styles.layout}>
+        <div className={styles.component}>
+          <Header />
+          <div className={styles.copy}>
+            사이토카인. <br />
+            <span>완벽한 탈모케어를 위한 선택</span>
+          </div>
+          <div className={styles.cardList}>
+            <CardList list={data} />
+          </div>
+          {modal && <Select />}
+          <div className={styles.toastLayout}>
+            <ToastContainer
+              className={styles.toast}
+              position="bottom-center"
+              limit={2}
+              autoClose={1000}
+              hideProgressBar={true}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
